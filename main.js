b1dc1cdac9d27b6af1d535b3fd819254fe7b0ee2
HM Rizwan Mazumder <42119262+xtremist96@users.noreply.github.com>
2020-08-23 21:40:53 +0530

    Add files via upload
    added printJs scripts and print button

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-var up = document.getElementById('GFG_UP');
+        var up = document.getElementById('GFG_UP');
         up.innerHTML = "Click on the btn to add image";
         var down = document.getElementById('GFG_DOWN');
+        var images = ["logo.jpg","logo.png"];
 
-        function GFG_Fun() {
+        function Fun() {
             var img = document.createElement('img');
             img.src = 'logo.jpg';
             document.getElementById('body').appendChild(img);
             down.innerHTML = "Image Added";
         }
+        function Print(){
+        }
+
+// var images = ["logo.jpg","logo.png"];
+// var target = document.getElementById('target');
+// function GFG_Fun() {
+// images.forEach(function(imgSrc){
+//     var newImg = document.createElement("img");
+//     newImg.src = imgSrc;
+//     target.appendChild(newImg);
+// })
+// }
